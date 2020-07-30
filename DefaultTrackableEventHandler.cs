06208e0e225e24f83dc8cec300883b2fd4944d46
I Gede Bintang Arya Budaya <47092602+bintangarb@users.noreply.github.com>
2020-07-31 07:40:01 +0800

    Add files via upload

---
+++ DefaultTrackableEventHandler.cs
@@ -0,0 +1,163 @@
+/*==============================================================================
+Copyright (c) 2019 PTC Inc. All Rights Reserved.
+
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using UnityEngine;
+using Vuforia;
+
+/// <summary>
+/// A custom handler that implements the ITrackableEventHandler interface.
+///
+/// Changes made to this file could be overwritten when upgrading the Vuforia version.
+/// When implementing custom event handler behavior, consider inheriting from this class instead.
+/// </summary>
+public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+    #region PROTECTED_MEMBER_VARIABLES
+
+    protected TrackableBehaviour mTrackableBehaviour;
+    protected TrackableBehaviour.Status m_PreviousStatus;
+    protected TrackableBehaviour.Status m_NewStatus;
+
+    public AudioSource asound;
+    public AudioClip aClip1;
+    public AudioClip aClip2;
+    public AudioClip aClip3;
+    public AudioClip aClip4;
+    public AudioClip aClip5;
+
+
+    #endregion // PROTECTED_MEMBER_VARIABLES
+
+    #region UNITY_MONOBEHAVIOUR_METHODS
+
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+    #endregion // UNITY_MONOBEHAVIOUR_METHODS
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        m_PreviousStatus = previousStatus;
+        m_NewStatus = newStatus;
+        
+        Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + 
+                  " " + mTrackableBehaviour.CurrentStatus +
+                  " -- " + mTrackableBehaviour.CurrentStatusInfo);
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackingFound();
+            if(mTrackableBehaviour.TrackableName=="KapalSelam2"){
+                asound.PlayOneShot (aClip1);
+            } else if (mTrackableBehaviour.TrackableName=="Venturi2"){
+                asound.PlayOneShot (aClip2);
+            } else if (mTrackableBehaviour.TrackableName=="SemprotanNyamuk2"){
+                asound.PlayOneShot (aClip3);
+            }else if (mTrackableBehaviour.TrackableName=="Peswat2"){
+                asound.PlayOneShot (aClip4);
+            } else {
+                asound.PlayOneShot (aClip5);
+            }
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            OnTrackingLost();
+
+             if(mTrackableBehaviour.TrackableName=="KapalSelam2"){
+                asound.Stop();
+            } else if (mTrackableBehaviour.TrackableName=="Venturi2"){
+               asound.Stop();
+            } else if (mTrackableBehaviour.TrackableName=="SemprotanNyamuk2"){
+               asound.Stop();
+            }else if (mTrackableBehaviour.TrackableName=="Peswat2"){
+               asound.Stop();
+            } else {
+                 asound.Stop();
+            }
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+    }
+
+    #endregion // PUBLIC_METHODS
+
+    #region PROTECTED_METHODS
+
+    protected virtual void OnTrackingFound()
+    {
+        if (mTrackableBehaviour)
+        {
+            var rendererComponents = mTrackableBehaviour.GetComponentsInChildren<Renderer>(true);
+            var colliderComponents = mTrackableBehaviour.GetComponentsInChildren<Collider>(true);
+            var canvasComponents = mTrackableBehaviour.GetComponentsInChildren<Canvas>(true);
+
+            // Enable rendering:
+            foreach (var component in rendererComponents)
+                component.enabled = true;
+
+            // Enable colliders:
+            foreach (var component in colliderComponents)
+                component.enabled = true;
+
+            // Enable canvas':
+            foreach (var component in canvasComponents)
+                component.enabled = true;
+        }
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        if (mTrackableBehaviour)
+        {
+            var rendererComponents = mTrackableBehaviour.GetComponentsInChildren<Renderer>(true);
+            var colliderComponents = mTrackableBehaviour.GetComponentsInChildren<Collider>(true);
+            var canvasComponents = mTrackableBehaviour.GetComponentsInChildren<Canvas>(true);
+
+            // Disable rendering:
+            foreach (var component in rendererComponents)
+                component.enabled = false;
+
+            // Disable colliders:
+            foreach (var component in colliderComponents)
+                component.enabled = false;
+
+            // Disable canvas':
+            foreach (var component in canvasComponents)
+                component.enabled = false;
+        }
+    }
+
+    #endregion // PROTECTED_METHODS
+}
---
+++ ToAnotherScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToAnotherScene : MonoBehaviour
+{
+   public void LoadSceneTentang () {
+        SceneManager.LoadScene ("Tentang");
+    }
+
+     public void LoadSceneMateri () {
+        SceneManager.LoadScene ("Materi");
+    }
+
+     public void LoadSceneBantuan () {
+        SceneManager.LoadScene ("Bantuan");
+    }
+
+    public void LoadSceneScanner () {
+        SceneManager.LoadScene ("Scanner");
+    }
+
+
+}
